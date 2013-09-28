5f41cd4865ab2dc0fdff88824f53eb4f67f1d68e
Justin R. Miller <incanus@codesorcery.net>
2013-09-28 10:03:29 -0500

    template cleanup

--- YouBeacon/YBViewController.m
+++ YouBeacon/YBViewController.m
@@ -8,22 +8,11 @@
 
 #import "YBViewController.h"
 
-@interface YBViewController ()
-
-@end
-
 @implementation YBViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
